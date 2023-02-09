6d57947ac9a4aae162864e241a36f9ed9ff4062b
ryvadev23 <ryvadev23@gmail.com>
2023-02-09 14:48:06 -0300

    meu primeiro comit

---
+++ 009.js
@@ -0,0 +1,12 @@
+var cla = 18
+console.log(`sua idade é ${cla}`)
+if ( cla < 16) {
+    console.log(' e vc nao Tem idade para ver esse filme')
+
+} else if ( cla <18 && cla <60){
+    console.log("parabens vc esta abto a ver esse filme")
+
+}
+ else {
+    console.log('esse filme nao é para vc vovo')
+}
---
+++ js.js
@@ -0,0 +1,6 @@
+var vel = 100
+console.log(`a velocidade do seu carro é ${vel}`)
+if (vel > 60) {
+  console.log(`vc ultrapassou o limite de velocidade. VC FOI MULTADO`)
+}
+console.log(`dirija usando cinto de segurança`)
